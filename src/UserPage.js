18080d58e49a5bda55be0e0d0df372dbe9cb048f
Xaroz <jasonguo2013jg@gmail.com>
2019-11-01 11:58:45 -0400

    Finished UserPage

--- src/UserPage.js
+++ src/UserPage.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function UserPage(props) {
   const username = props.match.params.user;
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [hasError, setHasError] = useState(null);
 
-  return (
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`https://api.github.com/users/${username}`);
+        setLoading(false);
+        setData(res.data);
+      } catch (err) {
+        setLoading(false);
+        setHasError(err);
+      }
+    };
+    fetchData();
+  }, [username]);
+
+  return loading ? (
     <div>
-      <h1>{username}</h1>
+      <h1>Loading...</h1>
+    </div>
+  ) : hasError ? (
+    <div>
+      <h1>User Not Found</h1>
+    </div>
+  ) : (
+    <div>
+      <h1>
+        {data.name} ({username})
+      </h1>
+      {/* relevant information to add: 
+        name:
+        username
+        avatar_url
+        bio
+        followers
+        following
+        url
+        public_repos */}
+      <img className="user-img" src={data.avatar_url} alt="user" />
     </div>
   );
 }
